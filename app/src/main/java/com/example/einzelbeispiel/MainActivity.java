1f2ef1892a7a26f0afd2950f4eb3472430d55ad3
majasmolej <smolej.9@gmail.com>
2024-03-11 13:15:02 +0100

    response from server redesign, added check for matriculation number

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.einzelbeispiel;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextNumber;
+    private TextView textViewResponse;
     private Button buttonSendToServer;
     private Button buttonDigitSum;
 
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         editTextNumber = findViewById(R.id.editTextNumber);
         buttonSendToServer = findViewById(R.id.button);
         buttonDigitSum = findViewById(R.id.button2);
+        textViewResponse = findViewById(R.id.textView2);
+
 
         // Set click listener for button
         buttonSendToServer.setOnClickListener(new View.OnClickListener() {
@@ -43,8 +47,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         buttonDigitSum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            @Override public void onClick(View view) {
                 calculateDigitSum();
             }
         });
@@ -61,7 +64,9 @@ public class MainActivity extends AppCompatActivity {
         ServerCommunication.sendMessageToServer(studentID, new ServerCommunication.ServerResponseListener() {
             @Override
             public void onResponseReceived(String response) {
-                Toast.makeText(MainActivity.this, "Server response: " + response, Toast.LENGTH_LONG).show();
+                String responseText = getString(R.string.server_response_text, response);
+                textViewResponse.setText(responseText);
+                //Toast.makeText(MainActivity.this, "Server response: " + response, Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -79,19 +84,29 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Bitte geben Sie Ihre Matrikelnummer ein.", Toast.LENGTH_SHORT).show();
         }
 
-        int sum = 0;
-        for(int i = 0; i < studentID.length(); i++){
-            char digitChar = studentID.charAt(i);
-            int digitValue = Character.getNumericValue(digitChar);
-            sum += digitValue;
-        }
+        if(isCorrectLength(studentID)){
+            int sum = 0;
+            for(int i = 0; i < studentID.length(); i++){
+                char digitChar = studentID.charAt(i);
+                int digitValue = Character.getNumericValue(digitChar);
+                sum += digitValue;
+            }
 
-        convertToBinary(sum);
+            convertToBinary(sum);
+
+        }else {
+            Toast.makeText(this, "Not a valid matriculation number.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void convertToBinary(int sum){
         String binaryString = Integer.toBinaryString(sum);
+        //textViewResponse.setText(binaryString);
         Toast.makeText(this, "The digit sum represented in binary is: " + binaryString, Toast.LENGTH_LONG).show();
+    }
+
+    public static boolean isCorrectLength(String input) {
+        return input.length() == 8;
     }
 
 }
